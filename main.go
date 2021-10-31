6af7503c1c16560585c0358e1b54021253cf04d4
uriehuang <uriehuang@tencent.com>
2021-10-31 17:32:58 +0800

    update

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
+	"log"
+
+	"go-homework/week2"
+	"go-homework/week3"
 	"go-homework/week9"
 )
 
 func main() {
-	//// 第二周作业
-	//_ = week2.HandleError()
-	//
-	//// 第三周作业
-	//s := week3.NewServer()
-	//cgiS := week3.NewCgiService()
-	//logS := week3.NewLogService()
-	//s.Register(cgiS.Name, &cgiS)
-	//s.Register(logS.Name, &logS)
-	//if err := s.Start(); err != nil {
-	//	log.Fatal(err)
-	//}
+	// 第二周作业
+	_ = week2.HandleError()
 
+	// 第三周作业
+	s := week3.NewServer()
+	cgiS := week3.NewCgiService()
+	logS := week3.NewLogService()
+	s.Register(cgiS.Name, &cgiS)
+	s.Register(logS.Name, &logS)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	// 第9周作业
 	week9.Serve()
 }
